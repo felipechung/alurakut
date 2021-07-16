935566e0d80274f12965a64a61f6a8a5438102a7
Felipe Chung <fchung.dev@gmail.com>
2021-07-16 14:42:23 -0300

    need to fix authentication

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,11 @@ import {
   AlurakutProfileSidebarMenuDefault,
   OrkutNostalgicIconSet,
 } from "../src/lib/AlurakutCommons";
+
 import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
+
+import jwt from "jsonwebtoken";
+import nookies from "nookies";
 
 function ProfileSidebar(props) {
   return (
@@ -51,10 +55,10 @@ function ProfileRelationsBox(props) {
   );
 }
 
-export default function Home() {
+export default function Home(props) {
   const [comunidades, setComunidades] = useState([]);
 
-  const randomUser = "felipechung";
+  const randomUser = props.githubUser;
 
   const pessoasFavoritas = [
     "juunegreiros",
@@ -212,4 +216,35 @@ export default function Home() {
       </MainGrid>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context);
+  const token = cookies.USER_TOKEN;
+
+  // const { isAuthenticated } = await fetch(
+  //   "https://alurakut.vercel.app/api/auth",
+  //   {
+  //     headers: {
+  //       Authorization: token,
+  //     },
+  //   }
+  // ).then((resposta) => resposta.json());
+
+  // if (!isAuthenticated) {
+  //   return {
+  //     redirect: {
+  //       destination: "/login",
+  //       permanent: false,
+  //     },
+  //   };
+  // }
+
+  const { githubUser } = jwt.decode(token);
+
+  return {
+    props: {
+      githubUser,
+    }, // will be passed to the page component as props
+  };
 }
